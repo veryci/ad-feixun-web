c728e3fe4e8c909cebbe6e5b633768a10dc1b92f
atory <atory_cc@163.com>
2018-08-21 18:40:10 +0800

    overview jia shuju

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const router = require('koa-router')({ prefix: '/api' });
 const moment = require('moment');
+const config = require('config');
+
 const DeviceModel = require('../models/DeviceModel');
+
+const during = config.get('during') || 10;
 
 require('moment-timezone');
 
@@ -31,13 +35,53 @@ router.get('/overview', async (ctx) => {
   const end = moment(endTime).endOf('day').toDate();
   const start = startTime ?
     moment(startTime).startOf('day').toDate() :
-    moment(end).subtract(10, 'day').startOf('day').toDate();
+    moment(end).subtract(during, 'day').startOf('day').toDate();
 
-  console.log(start, end);
+  let range = Math.floor(Math.abs(start - end) / 1000 / 60 / 60 / 24);
+  // console.log(range);
+  range = range < during ? during : range;
+  // console.log(start, end);
   // const data = await DeviceModel.search(start, end);
-
+  const today = moment().startOf('day');
+  const flow = {
+    today: 32,
+    totalNum: 3432,
+    chart: [],
+  };
+  const active = {
+    today: 32,
+    totalNum: 3432,
+    chart: [],
+  };
+  const online = {
+    today: 32,
+    totalNum: 3432,
+    chart: [],
+  };
+  for (let index = 0; index < range; index += 1) {
+    const date = today.clone().subtract(index, 'day').toDate();
+    const flowChartItem = {
+      date,
+      num: Math.floor(Math.random() * 100),
+    };
+    flow.chart.push(flowChartItem);
+    const activeChartItem = {
+      date,
+      num: Math.floor(Math.random() * 100),
+    };
+    active.chart.push(activeChartItem);
+    const onlineChartItem = {
+      date,
+      num: Math.floor(Math.random() * 100),
+    };
+    online.chart.push(onlineChartItem);
+  }
   // data
-  ctx.body = { };
+  ctx.body = {
+    flow,
+    active,
+    online,
+  };
 });
 
 
