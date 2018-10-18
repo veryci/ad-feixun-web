513eb566f0a8a7a696cb60cb26f0301955bdf969
梁琰沁 <1780304097@qq.com>
2018-10-18 15:45:12 +0800

    as;lf

--- src/containers/Region.js
+++ src/containers/Region.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import ReactEcharts from 'echarts-for-react';
-import geoCoord from './mapData';
+import geoCoord from '../lib/geoData';
 import { regionDataAction } from '../actions/region';
 
 require('echarts/map/js/china.js');
