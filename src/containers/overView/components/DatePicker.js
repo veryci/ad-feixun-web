e3886799be906aa89235f0c578b619e4e31b4c59
梁琰沁 <1780304097@qq.com>
2018-11-26 17:25:25 +0800

    asdlfjk

--- src/containers/overView/components/DatePicker.js
+++ src/containers/overView/components/DatePicker.js
@@ -2,38 +2,14 @@ import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Input, Button } from 'semantic-ui-react';
 
-class DatePicker extends Component {
-  constructor() {
-    super();
-    this.onSerch = this.onSerch.bind(this);
-  }
-  onSerch() {
-    const { startTime, endTime, changeTime } = this.props;
-    if (startTime && startTime <= endTime) {
-      changeTime(startTime, endTime);
-    } else {
-      alert('请输入正确的查询日期');
-    }
-  }
-  render() {
-    const { startTime, endTime, onChangeStart, onChangeEnd } = this.props;
-    return (
-      <Fragment>
-        <Input label={{ content: '开始日期', style: { lineHeight: '20px' } }} value={startTime} type="date" style={{ verticalAlign: 'middle', marginRight: '10px' }} onChange={(e, { value }) => onChangeStart(value)} />
-        <Input label={{ content: '结束日期', style: { lineHeight: '20px' } }} value={endTime} type="date" style={{ verticalAlign: 'middle', marginRight: '10px' }} onChange={(e, { value }) => onChangeEnd(value)} />
-        <Button
-          content="查询"
-          style={{ margin: '3px 0' }}
-          primary
-          onClick={this.onSerch}
-        />
-      </Fragment>
-    );
-  }
-}
+const DatePicker = ({ startTime, endTime, onChangeStart, onChangeEnd }) => (
+  <Fragment>
+    <Input label={{ content: '开始日期', style: { lineHeight: '20px' } }} value={startTime} type="date" style={{ verticalAlign: 'middle', marginRight: '10px' }} onChange={(e, { value }) => onChangeStart(value)} />
+    <Input label={{ content: '结束日期', style: { lineHeight: '20px' } }} value={endTime} type="date" style={{ verticalAlign: 'middle', marginRight: '10px' }} onChange={(e, { value }) => onChangeEnd(value)} />
+  </Fragment>
+);
 
 DatePicker.propTypes = {
-  changeTime: PropTypes.func.isRequired,
   startTime: PropTypes.string.isRequired,
   endTime: PropTypes.string.isRequired,
   onChangeStart: PropTypes.func.isRequired,
--- src/containers/overView/index.js
+++ src/containers/overView/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Container, Button } from 'semantic-ui-react';
+import { Container, Button, Input, Dropdown, Form } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import DatePicker from './components/DatePicker';
@@ -8,12 +8,19 @@ import GridDashboard from './components/GridDashboard';
 import Chart from './components/Chart';
 import { dailyDataAction } from '../../actions/dailyActive';
 
+const options = [
+  { key: 'edit', icon: 'edit', text: 'Edit Post', value: 'edit' },
+  { key: 'delete', icon: 'delete', text: 'Remove Post', value: 'delete' },
+  { key: 'hide', icon: 'hide', text: 'Hide Post', value: 'hide' },
+]
+
 class OverView extends React.Component {
   constructor() {
     super();
     this.state = {
       startTime: moment().subtract(8, 'days').format('YYYY-MM-DD'),
       endTime: moment().subtract(1, 'days').format('YYYY-MM-DD'),
+      code: '',
     };
   }
   componentDidMount() {
@@ -21,34 +28,54 @@ class OverView extends React.Component {
     const { startTime, endTime } = this.state;
     dispatch(dailyDataAction({ startTime, endTime }));
   }
+  onChangeCode = (e) => {
+    this.setState({ code: e.target.value });
+  }
   onChangeStart = (value) => {
     this.setState({ startTime: value });
   }
   onChangeEnd = (value) => {
     this.setState({ endTime: value });
   }
-  changeTime = (startTime, endTime) => {
+  onSerch = () => {
+    const { startTime, endTime } = this.state;
     const { dispatch } = this.props;
-    dispatch(dailyDataAction({ startTime, endTime }));
+    if (startTime && startTime <= endTime) {
+      dispatch(dailyDataAction({ startTime, endTime }));
+    } else {
+      alert('请输入正确的查询日期');
+    }
   }
   render() {
     const { startTime, endTime } = this.state;
     return (
       <React.Fragment>
         <Container style={{ marginTop: '7em' }}>
-          <DatePicker
-            startTime={startTime}
-            endTime={endTime}
-            changeTime={this.changeTime}
-            onChangeStart={this.onChangeStart}
-            onChangeEnd={this.onChangeEnd}
-          />&nbsp;&nbsp;
+          <Container style={{ marginBottom: '10px', textAlign: 'center' }} >
+            <Button.Group style={{ verticalAlign: 'middle', marginRight: '10px' }}>
+              <Button style={{ padding: '14px 21px 14px 21px'}}>版本号</Button>
+              <Dropdown options={options} style={{ padding: '14px 21px 14px 21px'}} floating button />
+            </Button.Group>
+            <Input label={{ content: '验证码', style: { lineHeight: '20px' } }} style={{ verticalAlign: 'middle', marginRight: '10px' }} onChange={this.onChangeCode} />
+            <DatePicker
+              startTime={startTime}
+              endTime={endTime}
+              onChangeStart={this.onChangeStart}
+              onChangeEnd={this.onChangeEnd}
+            />
+          </Container>
           <Button
             as="a"
             // href={`/api/overviewexcel?startTime=${startTime}&endTime=${endTime}`}
             content="导出Excel"
-            style={{ margin: '3px 0' }}
+            style={{float: 'right' }}
             primary
+          />
+          <Button
+            content="查询"
+            style={{ float: 'right', marginRight: '10px' }}
+            primary
+            onClick={this.onSerch}
           />
           <GridDashboard />
           <Chart />
