0c9984e84dc7d6a4c32f101e92e7b256e9009fa3
梁琰沁 <1780304097@qq.com>
2018-11-29 12:06:09 +0800

    asfasf

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -5,7 +5,7 @@ const { REACT_APP_VERSION } = process.env;
 
 function Footer() {
   return (
-    <Segment basic style={{ position: 'absolute', bottom: 0, padding: '1em 0em' }}>
+    <Segment basic style={{ position: 'fixed', bottom: 0, padding: '1em 0em' }}>
       <Container textAlign="right" >
         {`version:${REACT_APP_VERSION}`}
       </Container>
